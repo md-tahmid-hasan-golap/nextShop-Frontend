e943cc2c0e245395af894a0ae0ff07ec6b2b0e94
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-11-23 15:28:37 +0600

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h2 className="text-4xl font-bold">Next Shop Application</h2>
+    </div>
+  );
+}
